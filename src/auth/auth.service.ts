1d957010c13a68855e141214846ab442a739ef91
Brian Ricardo Tamin <19623154@mahasiswa.itb.ac.id>
2026-06-13 16:30:44 +0700

    feat(auth): add login endpoint and update auth DTOs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,22 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt'
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-	constructor(private prisma: PrismaService) {}
+	constructor(
+		private prisma: PrismaService,
+		private jwtService: JwtService,
+	) {}
 
 	async register(dto: RegisterDto) {
-
 		// check user exist in database?
 		const existingUser = await this.prisma.user.findUnique({
 			where: { email: dto.email },
 		});
-
 		if (existingUser) {
 			throw new ConflictException('Email already registered');
 		}
@@ -33,5 +36,27 @@ export class AuthService {
 		// exclude password
 		const { password, ...result }= user;
 		return result;
+	}
+
+	async login(dto: LoginDto) {
+		// ambil user from database
+		const user = await this.prisma.user.findUnique({
+			where: { email: dto.email },
+		});
+		if (!user){
+			throw new UnauthorizedException('Invalid credentials');
+		}
+
+		// check password matcch
+		const passwordMatch = await bcrypt.compare(dto.password, user.password);
+		if(!passwordMatch) {
+			throw new UnauthorizedException('Invalid credentials');
+		}
+
+		// bikin token
+		const payload = { sub: user.id, email: user.email };
+		return {
+			access_token: this.jwtService.sign(payload),
+		};
 	}
 }
